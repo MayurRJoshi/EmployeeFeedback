5f4e91b07c274a97c8613812077e6ce0f5e0ae22
Mayur Joshi <mayurjoshi2631993@gmail.com>
2015-11-13 16:46:52 +0530

    Remove useless

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 
-var assert = require('assert');
-
 var lib = require('../lib/lib');
 
-var collection;
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', lib.connectToDatabase , lib.connectToCollection , lib.insertToCollection);
+router.get('/', lib.insertToCollection);
 
 module.exports = router;
