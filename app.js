701cbb240b011b837252b0247414940af269e730
Mayur Joshi <mayurjoshi2631993@gmail.com>
2015-11-19 14:52:57 +0530

    Add logging with scribe-js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var scribe = require('scribe-js')();
 var routes = require('./routes/index');
 
 var app = express();
 
+app.use(scribe.express.logger());
+app.use('/logs' , scribe.webPanel());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
