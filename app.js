5b736c9325c160517de818edef129cb1e2cc0be5
Mayur Joshi <mayurjoshi2631993@gmail.com>
2015-11-19 16:02:15 +0530

    Add http-auth to scribe log webpanel

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var scribe = require('scribe-js')();
 var routes = require('./routes/index');
+var auth = require('http-auth');
 
 var app = express();
+var basic = auth.basic({
+	realm : 'Scribe log webpanel',
+	file : __dirname + '/users.htpasswd'
+});
 
 app.use(scribe.express.logger());
-app.use('/logs' , scribe.webPanel());
+app.use('/logs' , auth.connect(basic) , scribe.webPanel());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
