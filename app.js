f440ae6784a2ead3692bde6116cd628d490619d1
Mayur Joshi <mayurjoshi2631993@gmail.com>
2015-11-26 11:55:16 +0530

    Add pinging and scribe tags

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var scribe = require('scribe-js')();
+var console = process.console;
 var routes = require('./routes/index');
 var auth = require('http-auth');
 
@@ -8,6 +9,13 @@ var basic = auth.basic({
 	realm : 'Scribe log webpanel',
 	file : __dirname + '/users.htpasswd'
 });
+
+var https = require('https');
+setInterval(function (){
+	https.get('https://whispering-mesa-8158.herokuapp.com/ping' , function(){
+		console.tag('Ping').log('Success');
+	})
+} , 10000);
 
 app.use(scribe.express.logger());
 app.use('/logs' , auth.connect(basic) , scribe.webPanel());
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', lib.insertToCollection);
+router.get('/ping' , lib.ping);
+router.all('*' , function(req , res , next){
+	res.status(404).send('error');
+});
 
 module.exports = router;
